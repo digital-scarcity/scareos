5acb6a7fc0d0569d8e19f89971afa091fe7cd48b
max gravitt <max.gravitt@gmail.com>
2018-11-16 12:50:40 -0500

    update to latest version

--- listener/listener.js
+++ listener/listener.js
@@ -1,17 +1,18 @@
-var eosws       = require('eosws');
+var { EoswsClient, createEoswsSocket, InboundMessageType } = require( '@dfuse/eosws-js');
+
 var WebSocket   = require('ws');
 var axios       = require('axios');
 var figlet      = require('figlet');
 var config      = require ('config');
 
 const eosws_token = config.get('eosws_token');
-const eoswsAddress = config.get('eosws_address_prefix') + "?token=" + eosws_token;
 const origin = config.get('origin');
 const contract = config.get('listen_to_contract');
 const action = config.get('listen_to_action');
+const endpoint = config.get('eosws_endpoint');
 
-const ws = new WebSocket(eoswsAddress, {origin});
-const pr_req_id = eosws.generateReqId();
+const client = new EoswsClient(createEoswsSocket(() =>
+    new WebSocket(`wss://${endpoint}/v1/stream?token=${eosws_token}`, {origin})))
 
 figlet('EOS Listener', {
     font: "Big",
@@ -37,37 +38,33 @@ figlet('EOS Listener', {
 
 });
 
-ws.onopen = () => {
-    ws.send(eosws.get_actions(contract, action, contract, {req_id: pr_req_id}));
-};
-
-ws.onmessage = (message) => {
-    const pr_actions = eosws.parse_actions(message.data, pr_req_id);
-
-    if (pr_actions) {
-        var data = pr_actions.data.trace.act.data;
-        console.log ()
-        console.log (new Date().toISOString()) 
-        console.log (`-- Received event on ${contract}::${action} --`)
-        console.log (JSON.stringify(data, null, 4));
-      
-        if (config.has('post_to')) {
-            axios.post(config.get('post_to'), {
-                data
-            })
-            .then(function (response) {
-                console.log(response.status);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-        }
-        console.log ("Listening on EOS for: ");
-        console.log ("  Contract    :   ", contract);
-        console.log ("  Action      :   ", action);
-    }
-};
-
-
-
-
+client.connect().then(() => {
+    client
+        .getActionTraces({ account: contract, action_name: action })
+        .onMessage((message) => {
+            if (message.type === InboundMessageType.ACTION_TRACE) { 
+                var data = message.data.trace.act.data;
+                    console.log ()
+                    console.log (new Date().toISOString()) 
+                    console.log (`-- Received event on ${contract}::${action} --`)
+                    console.log (JSON.stringify(data, null, 4));
+                    
+                    if (config.has('post_to')) {
+                        axios.post(config.get('post_to'), {
+                            data
+                        })
+                        .then(function (response) {
+                            console.log(response.status);
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        });
+                    }
+                    console.log ("Listening on EOS for: ");
+                    console.log ("  Contract    :   ", contract);
+                    console.log ("  Action      :   ", action);
+            }
+        })
+}).catch((error) => {
+    console.log('Unable to connect to dfuse endpoint.', error)
+})
